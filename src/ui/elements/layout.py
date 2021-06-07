cb200204078b7e34dce44e7f937b687c830edbbc
itwasntzak <51720888+itwasntzak@users.noreply.github.com>
2021-06-07 10:52:19 -0600

    Small changes after working on mobile with app

--- src/ui/elements/layout.py
+++ src/ui/elements/layout.py
@@ -15,7 +15,7 @@ def button_group(button_list, label):
     return bootstrap.btn_group(
         content='\n'.join(button_list),
         label=label,
-        extra_classes='w-100',
+        extra_classes='w-100 py-1',
         vertical=True
     )
 
